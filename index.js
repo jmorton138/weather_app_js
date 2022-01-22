639176c7a22ddc9f5e2e051291923cb5726b95b0
jeff <jmorton138@gmail.com>
2022-01-22 14:40:37 +0000

    Skeleton changeBackground function

--- index.js
+++ index.js
@@ -7,7 +7,8 @@ async function getWeatherData(city) {
     try {
         const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&APPID=3b21d222488795c5ce2199c208c51c1b`, {mode: 'cors'});
         const weatherData = await response.json();
-        console.log(weatherData.weather[0]["main"]);
+        console.log(weatherData);
+        return weatherData.weather[0]["main"];
     } catch(err) {
         console.log(err);
     }
@@ -16,5 +17,10 @@ async function getWeatherData(city) {
 const displayWeatherData = () => {
     const city = getCity();
     getWeatherData(city);
+}
+
+const changeBackground = (weather) => {
+    const html = document.querySelector('html');
+    html.styles.background = "grey";
 }
 
