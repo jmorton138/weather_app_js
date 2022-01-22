bb138babb785272ad27c45dbc0257b1cceb83e0b
jeff <jmorton138@gmail.com>
2022-01-22 14:45:08 +0000

    Add empty named functions

--- index.js
+++ index.js
@@ -7,11 +7,15 @@ async function getWeatherData(city) {
     try {
         const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&APPID=3b21d222488795c5ce2199c208c51c1b`, {mode: 'cors'});
         const weatherData = await response.json();
-        console.log(weatherData);
+        console.log(weatherData.main);
         return weatherData.weather[0]["main"];
     } catch(err) {
         console.log(err);
     }
+}
+
+const processWeatherData = (weatherData) => {
+    
 }
 
 const displayWeatherData = () => {
@@ -22,5 +26,13 @@ const displayWeatherData = () => {
 const changeBackground = (weather) => {
     const html = document.querySelector('html');
     html.styles.background = "grey";
+}
+
+const toggleTemperatureUnits = () => {
+
+}
+
+const convertTemperature = () => {
+
 }
 
