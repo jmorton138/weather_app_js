722b7a7cdcac0ad8b6a7356f9a1359c6d1197466
jeff <jmorton138@gmail.com>
2022-01-22 14:51:44 +0000

    Add process weather function to pull necessary data for new weather object

--- index.js
+++ index.js
@@ -7,20 +7,25 @@ async function getWeatherData(city) {
     try {
         const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&APPID=3b21d222488795c5ce2199c208c51c1b`, {mode: 'cors'});
         const weatherData = await response.json();
-        console.log(weatherData.main);
-        return weatherData.weather[0]["main"];
+        return weatherData;
     } catch(err) {
         console.log(err);
     }
 }
 
 const processWeatherData = (weatherData) => {
-    
+    const weather = {
+        conditions: weatherData.weather[0]["main"],
+        temperature: weatherData.main.temp
+    }
+    console.log(weather);
+    return weather;
 }
 
-const displayWeatherData = () => {
-    const city = getCity();
-    getWeatherData(city);
+async function displayWeatherData() {
+    const city = await getCity();
+    const weather = await getWeatherData(city);
+    processWeatherData(weather);
 }
 
 const changeBackground = (weather) => {
